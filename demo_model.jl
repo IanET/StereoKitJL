1e932c2cccdb411285a810b7c27594c9e7b8de63
ianet <ianet@users.noreply.github.com>
2021-12-11 10:59:38 -1000

    Misc

--- demo_model.jl
+++ demo_model.jl
@@ -1,7 +1,6 @@
 import LibStereoKit as SK
-import Base.@kwdef
 import LibStereoKit: vec2, vec3, quat, color128, char16_t # Base SK types
-using Printf
+using Printf, IMisc
 
 Base.transcode(::Type{Cchar}, s::String) = reinterpret(Cchar, transcode(UInt8, s))
 macro u8_str(s) transcode(Cchar, s) end
@@ -31,7 +30,7 @@ const rval2 = Ref(0.5f0)
     fps::Float32 = 0
 end
 
-function updatefps(rs::RenderState)
+function updatefps(rs::RenderState)::Void
     rs.framecount += 1
     delta = time() - rs.frametime
     if delta > 1.0
@@ -39,10 +38,9 @@ function updatefps(rs::RenderState)
         rs.framecount = 0
         rs.frametime = time()
     end
-    return nothing
 end
 
-function render(rs::RenderState) 
+function render(rs::RenderState)::Void 
     head_pose = SK.input_head() |> unsafe_load
     window_pos = vec3(0, 0, -0.25)
     window_pose = SK.pose_t(window_pos, SK.quat_lookat(Ref(window_pos), Ref(head_pose.position)))
@@ -53,23 +51,21 @@ function render(rs::RenderState)
     pose = SK.matrix_trs(rs.helmet_pos, rs.helmet_ori, rs.helmet_scale)
     SK.model_draw(rs.helmet, pose, white, SK.render_layer_0)
     updatefps(rs)
-    return nothing
 end
 
 const grs = RenderState()
 const render_grs() = render(grs)
-const render_grs_c = @cfunction(render_grs, Nothing, ())
+const render_grs_c = @cfunction(render_grs, Void, ())
 
 sleep_optional(t) = t > 0 ? sleep(t) : nothing
 
-function renderloop(render_rs_c, sleeptime)
+function renderloop(render_rs_c, sleeptime)::Void
     while SK.sk_step(render_rs_c) > 0
         sleep_optional(sleeptime)
     end
-    return nothing
 end
 
-function initsk()
+function initsk()::Void
     settings = SK.sk_settings_t(
         pointer(appname),
         pointer(asset_folder),
@@ -80,23 +76,20 @@ function initsk()
         SK.log_diagnostic,
         0, 0, 0, 0, 0, 0, 0, C_NULL, C_NULL)
     SK.sk_init(settings) 
-    return nothing
 end
 
-function loadassets(rs::RenderState)
+function loadassets(rs::RenderState)::Void
     rs.ui_sprite = SK.sprite_create_file("StereoKitWide.png", SK.sprite_type_single, "default")
     rs.helmet = SK.model_create_file("DamagedHelmet.gltf", SK.shader_t(C_NULL))
-    return nothing
 end
 
-function main(rs::RenderState)
+function main(rs::RenderState)::Void
     initsk()
     loadassets(rs)
 
     renderloop(render_grs_c, isinteractive() ? 0.1 : 0.0)
 
     SK.sk_shutdown()
-    return nothing
 end
 
 main(grs)
